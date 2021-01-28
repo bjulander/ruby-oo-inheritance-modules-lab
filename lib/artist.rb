d5cb686a019166e252bec31c32b5cd72de057d07
Blake Julander <13lakejulander@gmail.com>
2021-01-28 13:45:46 -0700

    commit tree 92f95741749b49cbc8dd241ef22a0f4260311bd5

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,6 +1,10 @@
 require 'pry'
 
 class Artist
+  extend Memorable #::ClassMethods
+  extend Findable
+  include Paramable
+
   attr_accessor :name
   attr_reader :songs
 
@@ -11,9 +15,9 @@ class Artist
     @songs = []
   end
 
-  def self.find_by_name(name)
-    @@artists.detect{|a| a.name == name}
-  end
+  # def self.find_by_name(name)
+  #   @@artists.detect{|a| a.name == name}
+  # end
 
   def self.all
     @@artists
@@ -21,10 +25,6 @@ class Artist
 
   def self.reset_all
     self.all.clear
-  end
-
-  def self.count
-    self.all.count
   end
 
   def add_song(song)
@@ -36,7 +36,7 @@ class Artist
     songs.each { |song| add_song(song) }
   end
 
-  def to_param
-    name.downcase.gsub(' ', '-')
-  end
+  # def to_param
+  #   name.downcase.gsub(' ', '-')
+  # end
 end
